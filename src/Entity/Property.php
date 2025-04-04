62f7ca4b1f6ed86ef4491718a518a0b1e0c709b8
Harry EMARH <emarh.harry.code@gmail.com>
2025-04-04 23:51:45 +0200

    Property

---
+++ src/Entity/Property.php
@@ -0,0 +1,276 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PropertyRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PropertyRepository::class)]
+class Property
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $zipcode = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $country = null;
+
+    #[ORM\Column]
+    private ?float $latitude = null;
+
+    #[ORM\Column]
+    private ?float $longitude = null;
+
+    #[ORM\Column]
+    private ?float $pricePerNight = null;
+
+    #[ORM\Column]
+    private ?int $surface = null;
+
+    #[ORM\Column]
+    private ?int $numberOfBedrooms = null;
+
+    #[ORM\Column]
+    private ?int $numberOfBeds = null;
+
+    #[ORM\Column]
+    private ?int $numberOfBathrooms = null;
+
+    #[ORM\Column]
+    private array $amenities = [];
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $rules = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $isAvailable = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZipcode(): ?string
+    {
+        return $this->zipcode;
+    }
+
+    public function setZipcode(string $zipcode): static
+    {
+        $this->zipcode = $zipcode;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): static
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): static
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getPricePerNight(): ?float
+    {
+        return $this->pricePerNight;
+    }
+
+    public function setPricePerNight(float $pricePerNight): static
+    {
+        $this->pricePerNight = $pricePerNight;
+
+        return $this;
+    }
+
+    public function getSurface(): ?int
+    {
+        return $this->surface;
+    }
+
+    public function setSurface(int $surface): static
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getNumberOfBedrooms(): ?int
+    {
+        return $this->numberOfBedrooms;
+    }
+
+    public function setNumberOfBedrooms(int $numberOfBedrooms): static
+    {
+        $this->numberOfBedrooms = $numberOfBedrooms;
+
+        return $this;
+    }
+
+    public function getNumberOfBeds(): ?int
+    {
+        return $this->numberOfBeds;
+    }
+
+    public function setNumberOfBeds(int $numberOfBeds): static
+    {
+        $this->numberOfBeds = $numberOfBeds;
+
+        return $this;
+    }
+
+    public function getNumberOfBathrooms(): ?int
+    {
+        return $this->numberOfBathrooms;
+    }
+
+    public function setNumberOfBathrooms(int $numberOfBathrooms): static
+    {
+        $this->numberOfBathrooms = $numberOfBathrooms;
+
+        return $this;
+    }
+
+    public function getAmenities(): array
+    {
+        return $this->amenities;
+    }
+
+    public function setAmenities(array $amenities): static
+    {
+        $this->amenities = $amenities;
+
+        return $this;
+    }
+
+    public function getRules(): ?string
+    {
+        return $this->rules;
+    }
+
+    public function setRules(?string $rules): static
+    {
+        $this->rules = $rules;
+
+        return $this;
+    }
+
+    public function isAvailable(): ?bool
+    {
+        return $this->isAvailable;
+    }
+
+    public function setIsAvailable(?bool $isAvailable): static
+    {
+        $this->isAvailable = $isAvailable;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
